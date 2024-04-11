bdd414abb5c8ed06a29716f034d80649349b4749
Valentin Nuñez <valenunez133@gmail.com>
2024-04-11 15:56:14 -0300

    Solucion de errores de estilo

--- Modulos/Funciones.py
+++ Modulos/Funciones.py
@@ -1,15 +1,14 @@
-
 def generate_player_stats(names,goals,goals_avoided,assist):
     """ Genera una lista de diccionarios con las estadisticas de los jugadores.
 
       Args:
-         names: Lista con nombre de jugadores
-        goals: Una lista con la cantidad de goles de cada jugador
-         goals_avoided: Una lista con la cantidad de goles evitados por jugador
-         assist: Una lista con la cantidad de goles asistidos por jugador
+         names(list): Lista con nombre de jugadores
+         goals(list): Una lista con la cantidad de goles de cada jugador.
+         goals_avoided(list): Una lista con la cantidad de goles evitados por jugador.
+         assist(list): Una lista con la cantidad de goles asistidos por jugador.
 
       Returns:
-         return: tipo diccionario """
+         List: Lista de diccionarios con las estadisticas de cada uno de los jugadores. """
     
     player_stats=[]
     for i in range(len(names)):
@@ -22,9 +21,9 @@ def find_goleador (players_stats):
     """ Encuentra en la lista de diccionarios al jugador con mayor cantidad de goles.
 
     Args:
-        players_stats: parametro de tipo lista, la cual contiene diccionarios. Debe contener la clave goals.
+        players_stats(list): Lista, la cual contiene diccionarios. Debe contener la clave goals.
     Returns: 
-        goleador:un unico elemento de tipo diccionario que representa al goleador
+        goleador(dict): Diccionario que representa al goleador.
     """
     goleador=max(players_stats, key=lambda x: x['goals'])
     return goleador
@@ -33,32 +32,34 @@ def most_influential(players_stats):
     """ Encuentra al jugador más influyente, multiplicando sus estadisticas utilizando una tabla de valores dada. 
 
     Args:
-        player_stats: parametro de tipo lista, la cual contiene diccionarios. Debe contener la clave goals, goals_avoided y assist
+        players_stats(list): Lista, la cual contiene diccionarios. Debe contener la clave goals, goals_avoided y assist.
 
     Returns: 
-        player: un unico elemento de tipo diccionario que representa al jugador más influyente.
+        player(dict): Diccionario que representa al jugador más influyente.
     """
     player= max(players_stats, key=lambda x: x["goals"]*1.5 + x["goals_avoided"]*1.25 + x["assist"])
     return player
 
-def get_promedio(players_stats):
+def get_promedio(players_stats,games):
     """ Calcula el promedio de los goles del equipo.
 
     Args:
-        player_stats: parametro de tipo lista, la cual contiene diccionarios. Debe contener la clave goals.
+        players_stats(list): parametro el cual contiene diccionarios. Debe contener la clave goals.
+        games(integer): parametro que contiene la cantidad de partidos jugados.
     
     Returns:
-        total_goals/25: Devuelve un elemento de tipo float.
+        float: Devuelve el promedio de goles en el equipo.
     """
     total_goals= sum(player["goals"] for player in players_stats)
-    return total_goals/25
+    return total_goals/games
 
-def get_promedio_goleador(goals):
-    """ Calcula el promedio de los goles del equipo.
+def get_promedio_goleador(goals,games):
+    """ Calcula el promedio de los goles del goleador.
     Args:
-        goals: parametro de tipo integer
+        goals(integer): parametro que contiene los goles del goleador.
+        games(integer): parametro que contiene la cantidad de partidos jugados.
     
     Returns:
-        total_goals/25: Devuelve un elemento de tipo float.
+        float: Devuelve el promedio de goles por patidos del jugador.
     """
-    return goals/25
+    return goals/games
